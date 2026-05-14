31a63dfe4a30041a8d218c6a1cfe70187ae98374
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-14 17:44:25 +0530

    Add RegisterRequest DTO with validation annotations

---
+++ src/main/java/com/sravan/Coding_Platform/dto/RegisterRequest.java
@@ -0,0 +1,46 @@
+package com.sravan.Coding_Platform.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+public class RegisterRequest {
+    @NotBlank(message = "Name is Required")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Size(min = 6,message = "Password must be atleast 6 characters")
+    private String password;
+
+    public RegisterRequest(){};
+    public RegisterRequest(String name, String email, String password){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+}
