1e2ab9bd4e0a18843e2e9cf153c5d77fee978284
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-15 16:59:10 +0530

    Added a method for checking if the login password matches with the database one

--- src/main/java/com/sravan/Coding_Platform/service/UserService.java
+++ src/main/java/com/sravan/Coding_Platform/service/UserService.java
@@ -1,23 +1,45 @@
 package com.sravan.Coding_Platform.service;
 
+import com.sravan.Coding_Platform.dto.LoginRequest;
 import com.sravan.Coding_Platform.dto.RegisterRequest;
 import com.sravan.Coding_Platform.model.User;
 import com.sravan.Coding_Platform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public User registerUser(RegisterRequest request){
         User user = new User();
 
         user.setName(request.getName());
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole("USER");
         return userRepository.save(user);
     }
+
+    public String loginUser(LoginRequest request){
+        Optional<User> optionalUser = userRepository.findByEmail(request.getEmail());
+        if(optionalUser.isEmpty()){
+            return "User nor found!";
+        }
+        User user = optionalUser.get();
+
+        boolean isPasswordMatch = passwordEncoder.matches(request.getPassword(), user.getPassword());
+        if(!isPasswordMatch){
+            return "Incorrect Password.";
+        }
+        return "Login Successful";
+    }
 }
+
+
