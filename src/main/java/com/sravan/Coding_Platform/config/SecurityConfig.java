b18dfe06790a4f11d9b7c6fd9d1679b65eb72836
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-15 14:46:51 +0530

    Configured Spring Security for public endpoints

---
+++ src/main/java/com/sravan/Coding_Platform/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.sravan.Coding_Platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+}
